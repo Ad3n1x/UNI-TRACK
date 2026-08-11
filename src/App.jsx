e3e6f11f7c5a70a1b6aec38b86ba0b87dfb7a38b
Ad3n1x <adenix271@gmail.com>
2026-08-11 20:36:04 +0100

    style: format and organize JSX elements for improved readability in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Auth from './components/auth/Auth';
+import LoginForm from './components/auth/LoginForm';
+import RegisterForm from './components/auth/RegisterForm';
+
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="/auth" element={<Auth />}>
-          <Route path="login" element={<LoginForm />} />
-          <Route path="register" element={<RegisterForm />} />
-        </Route>
+    <Routes>
+      {/* Home page */}
+      <Route index element={<Homepage />} />
 
+      {/* Auth routes */}
+      <Route path="auth" element={<Auth />}>
+        <Route path="login" element={<LoginForm />} />
+        <Route path="register" element={<RegisterForm />} />
+      </Route>
 
-        {/* wild card routing */}
-        {/* <Route path='*' element={<Notfound/>}/> */}
-      </Routes>
+      {/* Wildcard route for 404 */}
+      {/* <Route path="*" element={<Notfound />} /> */}
+    </Routes>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
