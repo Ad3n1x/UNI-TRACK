66aaf5fa973f6e333fbfc9e66f4b82b7e1536ea3
Ad3n1x <adenix271@gmail.com>
2026-08-24 20:15:34 +0100

    feat: add ToastContainer for global notifications in main component

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,26 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+// 1. Import Toastify CSS and container
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <App />
+      {/* 2. Add the ToastContainer here so it's global */}
+      <ToastContainer 
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </StrictMode>
   </BrowserRouter>,
 )
