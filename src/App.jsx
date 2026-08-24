05b26b503997e4c12f8dcde9ccf83ed5a2a0f774
Ad3n1x <adenix271@gmail.com>
2026-08-24 20:03:52 +0100

    feat: integrate react-toastify for global notifications in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // 👈 Required for styling
 import Homepage from "./pages/Homepage";
 import LoginForm from "./components/auth/LoginForm";
 import RegisterForm from "./components/auth/RegisterForm";
@@ -7,21 +9,30 @@ import NotFound from "./pages/Notfound";
 
 export default function App() {
   return (
-    <Routes>
-      {/* Top-Level Routes */}
-      <Route index element={<LoginForm />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/homepage" element={<Homepage />} />
+    <>
+      <Routes>
+        {/* Top-Level Routes */}
+        <Route index element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/homepage" element={<Homepage />} />
 
+        {/* Catch-All 404 Route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
-      {/* Catch-All 404 Route */}
-      <Route
-        path="*"
-        element={
-          <NotFound />
-        }
+      {/* 👈 Required to actually display the toasts globally */}
+      <ToastContainer 
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
       />
-    </Routes>
+    </>
   );
 }
